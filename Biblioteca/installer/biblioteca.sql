b1a0983b733d199878dca224957b31426afbb47a
MrTacod <akonmelilla@gmail.com>
2023-03-10 19:24:38 +0100

    10/03 19:30

--- Biblioteca/installer/biblioteca.sql
+++ Biblioteca/installer/biblioteca.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost:3306
--- Tiempo de generación: 24-02-2023 a las 20:49:01
+-- Tiempo de generación: 10-03-2023 a las 19:14:03
 -- Versión del servidor: 8.0.31
 -- Versión de PHP: 8.0.25
 
@@ -28,10 +28,22 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `contacto` (
-    `id` int NOT NULL,
-    `id_usuario` int NOT NULL,
-    `correo` varchar(255) NOT NULL,
-    `mensaje` varchar(255) NOT NULL
+  `id` int NOT NULL,
+  `id_usuario` int NOT NULL,
+  `correo` varchar(255) NOT NULL,
+  `mensaje` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `curso`
+--
+
+CREATE TABLE `curso` (
+  `id` int NOT NULL,
+  `id_libro` int NOT NULL,
+  `curso` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
@@ -41,13 +53,13 @@ CREATE TABLE `contacto` (
 --
 
 CREATE TABLE `libro` (
-    `id` int NOT NULL,
-    `imagen` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-    `titulo` varchar(255) NOT NULL,
-    `autor` varchar(255) NOT NULL,
-    `isbn` varchar(100) NOT NULL,
-    `resumen` varchar(255) NOT NULL,
-    `formato` varchar(255) NOT NULL
+  `id` int NOT NULL,
+  `imagen` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `titulo` varchar(255) NOT NULL,
+  `autor` varchar(255) NOT NULL,
+  `isbn` varchar(100) NOT NULL,
+  `resumen` varchar(255) NOT NULL,
+  `formato` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
@@ -55,10 +67,35 @@ CREATE TABLE `libro` (
 --
 
 INSERT INTO `libro` (`id`, `imagen`, `titulo`, `autor`, `isbn`, `resumen`, `formato`) VALUES
-(1, 'chica-nieve.png', 'La chica de nieve', 'Javier Castillo', 9788491292661, 'En la cabalgata de Acción de Gracias de 1998, Kiera Templeton desaparece y, años después, sus padres reciben un paquete con una cinta VHS de Kiera con 8 años jugando en una habitación desconocida.', 'original'),
-(2, 'bestia.png', 'La bestia', 'Carmen Mola', 9788408249849, 'En 1834, Madrid sufre una epidemia de cólera y la aparición de cadáveres desmembrados de niñas que nadie reclama. La Bestia es el principal sospechoso, pero nadie lo ha visto.', 'original'),
-(3, 'magia-orden.png', 'La magia del orden', 'Marie Kondo', 9781607747307, 'Es una guía práctica para organizar y simplificar tu vida. Kondo comparte su método KonMari, que se enfoca en conservar sólo lo que te hace feliz y deshacerte de todo lo demás.', 'fotocopiado'),
-(4, 'mercader-libros.png', 'El mercader de libros', 'Luis Zueco', 9788466667005, 'Es una novela de ficción histórica ambientada en la España del siglo XV. La historia sigue la vida de un vendedor de libros judío, Moshe, quien debe navegar por los peligros de la Inquisición y la intriga política de la época.', 'fotocopiado');
+(1, 'chica-nieve.png', 'La chica de nieve', 'Javier Castillo', '9788491292661', 'En la cabalgata de Acción de Gracias de 1998, Kiera Templeton desaparece y, años después, sus padres reciben un paquete con una cinta VHS de Kiera con 8 años jugando en una habitación desconocida.', 'original'),
+(2, 'bestia.png', 'La bestia', 'Carmen Mola', '9788408249849', 'En 1834, Madrid sufre una epidemia de cólera y la aparición de cadáveres desmembrados de niñas que nadie reclama. La Bestia es el principal sospechoso, pero nadie lo ha visto.', 'original'),
+(3, 'magia-orden.png', 'La magia del orden', 'Marie Kondo', '9781607747307', 'Es una guía práctica para organizar y simplificar tu vida. Kondo comparte su método KonMari, que se enfoca en conservar sólo lo que te hace feliz y deshacerte de todo lo demás.', 'fotocopiado'),
+(4, 'mercader-libros.png', 'El mercader de libros', 'Luis Zueco', '9788466667005', 'Es una novela de ficción histórica ambientada en la España del siglo XV. La historia sigue la vida de un vendedor de libros judío, Moshe, quien debe navegar por los peligros de la Inquisición y la intriga política de la época.', 'fotocopiado');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tema`
+--
+
+CREATE TABLE `tema` (
+  `id` int NOT NULL,
+  `id_libro` int NOT NULL,
+  `tema` varchar(255) NOT NULL,
+  `desc_tema` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `ubicacion`
+--
+
+CREATE TABLE `ubicacion` (
+  `id` int NOT NULL,
+  `id_libro` int NOT NULL,
+  `ubicacion` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -67,13 +104,13 @@ INSERT INTO `libro` (`id`, `imagen`, `titulo`, `autor`, `isbn`, `resumen`, `form
 --
 
 CREATE TABLE `usuario` (
-    `id` int NOT NULL,
-    `nombre` varchar(255) NOT NULL,
-    `apellido` varchar(255) NOT NULL,
-    `correo` varchar(255) NOT NULL,
-    `contrasena` varchar(100) NOT NULL,
-    `telefono` int NOT NULL,
-    `rol` varchar(255) NOT NULL
+  `id` int NOT NULL,
+  `nombre` varchar(255) NOT NULL,
+  `apellido` varchar(255) NOT NULL,
+  `correo` varchar(255) NOT NULL,
+  `contrasena` varchar(100) NOT NULL,
+  `telefono` int NOT NULL,
+  `rol` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
@@ -84,21 +121,42 @@ CREATE TABLE `usuario` (
 -- Indices de la tabla `contacto`
 --
 ALTER TABLE `contacto`
-    ADD PRIMARY KEY (`id`),
-    ADD KEY `id_usuario` (`id_usuario`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_usuario` (`id_usuario`);
+
+--
+-- Indices de la tabla `curso`
+--
+ALTER TABLE `curso`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_libro` (`id_libro`);
 
 --
 -- Indices de la tabla `libro`
 --
 ALTER TABLE `libro`
-    ADD PRIMARY KEY (`id`),
-    ADD UNIQUE KEY `isbn` (`isbn`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `isbn` (`isbn`);
+
+--
+-- Indices de la tabla `tema`
+--
+ALTER TABLE `tema`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_libro` (`id_libro`);
+
+--
+-- Indices de la tabla `ubicacion`
+--
+ALTER TABLE `ubicacion`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_libro` (`id_libro`);
 
 --
 -- Indices de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-    ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
@@ -111,16 +169,34 @@ ALTER TABLE `contacto`
   MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT de la tabla `curso`
+--
+ALTER TABLE `curso`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT de la tabla `libro`
 --
 ALTER TABLE `libro`
-    MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de la tabla `tema`
+--
+ALTER TABLE `tema`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `ubicacion`
+--
+ALTER TABLE `ubicacion`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-    MODIFY `id` int NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
 --
 -- Restricciones para tablas volcadas
@@ -130,7 +206,25 @@ ALTER TABLE `usuario`
 -- Filtros para la tabla `contacto`
 --
 ALTER TABLE `contacto`
-    ADD CONSTRAINT `contacto_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `contacto_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `curso`
+--
+ALTER TABLE `curso`
+  ADD CONSTRAINT `curso_ibfk_1` FOREIGN KEY (`id_libro`) REFERENCES `libro` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `tema`
+--
+ALTER TABLE `tema`
+  ADD CONSTRAINT `tema_ibfk_1` FOREIGN KEY (`id_libro`) REFERENCES `libro` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `ubicacion`
+--
+ALTER TABLE `ubicacion`
+  ADD CONSTRAINT `ubicacion_ibfk_1` FOREIGN KEY (`id_libro`) REFERENCES `libro` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
